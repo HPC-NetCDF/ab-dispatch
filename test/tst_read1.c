2573059a477bc9e229b17403ea6f653f409af85d
Ed Hartnett <edwardjameshartnett@gmail.com>
2018-02-05 09:11:36 -0700

    MY CREATION LIVES!!!!!

--- test/tst_read1.c
+++ test/tst_read1.c
@@ -15,12 +15,25 @@ extern NC_Dispatch AB_dispatcher;
 int
 main()
 {
+   int ncid;
+   int ndims, nvars, natts, unlimdimid;
    int ret;
    
-   printf("howdy!\n");
+   printf("\nTesting AB format dispatch layer...");
    if ((ret = nc_def_user_format(NC_UF0, &AB_dispatcher, NULL)))
       return ret;
-   
+
+   if ((ret = nc_open(TEST_FILE, NC_UF0, &ncid)))
+      return ret;
+   if ((ret = nc_inq(ncid, &ndims, &nvars, &natts, &unlimdimid)))
+      return ret;
+   /* printf("ndims %d nvars %d natts %d unlimdimid %d\n", ndims, nvars, natts, unlimdimid); */
+   if (ndims != 4 || nvars != 6 ||natts !=10 || unlimdimid != -1)
+       return 111;
+   if ((ret = nc_close(ncid)))
+      return ret;
+
+   printf("SUCCESS!\n");
    return 0;
 }
 
